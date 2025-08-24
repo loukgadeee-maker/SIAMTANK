b866256753f4b49ffc8e1273f4d97a8b1a49a4da
loukgadeee-maker <loukgadeee@gmail.com>
2025-08-24 17:10:24 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,15 @@
+// ประกาศค่า 
+
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+const WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+const APPROVED_WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+
+//LIFF_ID
+const LIFF_ID = 'XXXXXXXXX'; // LIFF ID Checkin
+const LIFF_ID_HISTORY = 'XXXXXXXXX'; // LIFF ID History
+const LIFF_ID_LEAVE = 'XXXXXXXXX'; // LIFF ID Leave
+const LIFF_ID_OT = 'XXXXXXXXX'; // LIFF ID Ot
+const LIFF_ID_REGISTER = 'XXXXXXXXX'; // LIFF ID Register
