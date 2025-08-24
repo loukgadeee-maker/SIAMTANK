86df9949c8d404ef0e190de31f5633d9c152934a
loukgadeee-maker <loukgadeee@gmail.com>
2025-08-24 17:20:23 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,15 +1,16 @@
 // ประกาศค่า 
 
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const WEB_APP_OT_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const APPROVED_WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwUYsoYp2HPKyH2nuemo7T82jwKpW4vfa3GnhEAFYU5FAC28eRX1jlsAwi2k3Q139B9/exec';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbzQkPoNa_IBRLDhhiqEqFSIG-Y_Z8Ph3TP9iecHM5fzEUl--Mdl8oFGx2KkDApW0SOK/exec';
+const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbwd_X5qmr3xDy0MQujYREkT-A6czORJbD8Yt_NPK5sayWB9Ktr8sq7bT3Szxk8GNcwv/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbyIuJc0G09ifO_fFP1jvXWLg_OQDgCv82mzxdZHU2LKHfh89lflXrJRzXh9wsSc8VP_/exec';
+const APPROVED_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxy23cS9pj6og9Ri2pwkMMfg7pq10lX8cIfHm-AHgvKieGL0HqLR3IEbpNwUsTUn_Fm_g/exec';
 
 //LIFF_ID
-const LIFF_ID = 'XXXXXXXXX'; // LIFF ID Checkin
-const LIFF_ID_HISTORY = 'XXXXXXXXX'; // LIFF ID History
-const LIFF_ID_LEAVE = 'XXXXXXXXX'; // LIFF ID Leave
-const LIFF_ID_OT = 'XXXXXXXXX'; // LIFF ID Ot
-const LIFF_ID_REGISTER = 'XXXXXXXXX'; // LIFF ID Register
+const LIFF_ID = '2007981263-7QzJ0zl6'; // LIFF ID Checkin
+const LIFF_ID_HISTORY = '2007981263-Ge62W6Eo'; // LIFF ID History
+const LIFF_ID_LEAVE = '2007981263-ljV4BVdz'; // LIFF ID Leave
+const LIFF_ID_OT = '2007981263-dY8gp897'; // LIFF ID Ot
+
+const LIFF_ID_REGISTER = '2007981263-lRb7zbqg'; // LIFF ID Register
